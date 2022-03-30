b6ea5d3067403fd18a5a1318c65ad3e6c8cbf3ec
=Eric Niemeyer <=niemeyer.eric@gmail.com>
2022-03-30 11:17:35 -0400

    done

--- clockangles.js
+++ clockangles.js
@@ -16,3 +16,29 @@ p(clockAngles(a1));
 // return Math.abs(mAngle - hAngle) > 180
 // ? Math.abs(mAngle - hAngle) - 180
 // : Math.abs(mAngle - hAngle);
+
+// const clock = () => {
+//   minutes = 0;
+//   var mAngle = (minutes * 6) % 360;
+//   var hAngle = ((minutes * 6) / 12) % 360;
+//   var hh = "";
+//   var mm = "";
+
+// //   while (1) {
+//     minutes++;
+//     mAngle = (minutes * 6) % 360;
+//     hAngle = ((minutes * 6) / 12) % 360;
+//     p("____");
+//     for (var h = 0; h < hAngle; h++) {
+//       hh += "|";
+//     }
+//     p(hh);
+//     for (var m = 0; m < mAngle; m++) {
+//       mm += "|";
+//     }
+//     p(mm);
+//     p("____");
+// //   }
+// };
+
+// clock();
